4e5692b65a54e2df903f23431ed96ab156fe6e8e
granitnebiu <granitnebiu@gmail.com>
2022-06-15 19:59:22 +0200

    handdling api request error with React Query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,13 +2,17 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const fetchSuperHeros = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroes1");
 };
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-heros", fetchSuperHeros);
+  const { isLoading, data, isError, error } = useQuery("super-heros", fetchSuperHeros);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
