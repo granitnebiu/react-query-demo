eccbda8b0c0560e3f7dcc4cee6fbcab628f02b42
granitnebiu <granitnebiu@gmail.com>
2022-06-15 19:54:20 +0200

    ImpleatedReact Query in RQSuperHeros

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,22 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeros = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>;
+  const { isLoading, data } = useQuery("super-heros", fetchSuperHeros);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heros Page </h2>
+      {data.data.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </>
+  );
 };
